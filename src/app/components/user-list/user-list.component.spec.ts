266cbbd237026ef33f506f2330faec5d717e5a04
5DaysCode <5DaysCode@gmail.com>
2023-07-06 13:44:00 +0200

    functional tests

--- src/app/components/user-list/user-list.component.spec.ts
+++ src/app/components/user-list/user-list.component.spec.ts
@@ -1,27 +1,29 @@
-// UserListComponent.spec.ts
-
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { UserListComponent } from './user-list.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
-import { Store } from '@ngrx/store';
+
 import { LoadUsersAction } from '../../state/actions/user.actions';
 import { User } from 'src/app/models/user.model';
-import { of } from 'rxjs';
-import { fakeAsync, tick } from '@angular/core/testing';
 
 const mockUsers: User[] = [
   {
     id: 1,
     name: 'User 1',
-    email: 'user1@email.com',
+    email: 'user1@example.com',
     username: 'user1',
     password: 'pass1',
   },
   {
     id: 2,
     name: 'User 2',
-    email: 'user2@email.com',
+    email: 'user2@example.com',
     username: 'user2',
     password: 'pass2',
   },
@@ -30,19 +32,13 @@ const mockUsers: User[] = [
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
-  let store: MockStore;
-  let dispatchSpy: any;
-  let selectSpy: any;
-  const initialState = { users: [] }; // Initialize to the state shape of users...
+  let store: MockStore<any>;
+  const initialState = { users: [] };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [UserListComponent],
-      providers: [
-        provideMockStore({ initialState }),
-        // Include this:
-        { provide: Store, useClass: MockStore },
-      ],
+      providers: [provideMockStore({ initialState })],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
@@ -52,21 +48,7 @@ describe('UserListComponent', () => {
     component = fixture.componentInstance;
 
     store = TestBed.inject(MockStore);
-    dispatchSpy = jest.spyOn(store, 'dispatch').mockImplementation(() => {});
-
-    selectSpy = jest.spyOn(store, 'select').mockImplementation((selector) => {
-      console.log('store.select is being called with:', selector);
-      if (typeof selector === 'string') {
-        console.log('string selector');
-        return of({ users: mockUsers });
-      } else if (typeof selector === 'function') {
-        console.log('function selector');
-        return of(selector({ users: mockUsers }));
-      } else {
-        console.log('Unexpected selector type');
-        throw new Error('Unexpected selector type');
-      }
-    });
+    store.setState({ users: mockUsers });
 
     fixture.detectChanges();
   });
@@ -75,33 +57,24 @@ describe('UserListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should dispatch the load users action on init', () => {
-    store.dispatch(new LoadUsersAction());
+  it('should dispatch the load users action on init', fakeAsync(() => {
+    const dispatchSpy = jest.spyOn(store, 'dispatch');
+    component.ngOnInit();
+    tick();
     expect(dispatchSpy).toHaveBeenCalledWith(new LoadUsersAction());
-  });
+  }));
 
-  // it('should select users state', fakeAsync(() => {
-  //   fixture.detectChanges();
-  //   expect(selectSpy).toHaveBeenCalledWith('users');
-
-  //   tick();
-
-  //   fixture.detectChanges();
-
-  //   let usersResult: User[] | undefined;
-  //   component.users$?.subscribe((users) => {
-  //     usersResult = users;
-  //   });
-
-  //   tick();
-
-  //   expect(usersResult).toEqual(mockUsers);
-  // }));
+  it('should select users state', fakeAsync(() => {
+    component.ngOnInit();
+    tick();
+    if (component.users$) {
+      component.users$.toPromise().then((users) => {
+        expect(users).toEqual(mockUsers);
+      });
+    }
+  }));
 
   it('should have users observable', (done) => {
-    //mocking the store to return  predifined  users
-    store.setState({ users: mockUsers });
-
     component.users$?.subscribe((users) => {
       expect(users).toEqual(mockUsers);
       done();
